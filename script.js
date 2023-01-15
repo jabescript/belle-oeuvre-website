baeba75a8e1f5befb61a7073b2536bbbb6760e4a
Jabez Bismanos <jabezbismanos0@gmail.com>
2023-01-15 22:39:46 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$( document ).ready(function() {
+  new WOW().init();
+});
+
+
+$( ".wow" ).addClass( "fadeInUp" );
+
+
+$(window).on("load",function(){
+	$(".loader-wrapper").fadeOut("slow");
+})
+
